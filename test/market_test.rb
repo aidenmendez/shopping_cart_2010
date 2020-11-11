d245dc5a544ce05919c0f9e0063455beb983af1c
Aiden Mendez <aidenmendez@gmail.com>
2020-11-11 09:59:01 -0700

    Test venodrs_that_sell_method

--- test/market_test.rb
+++ test/market_test.rb
@@ -48,5 +48,13 @@ class MarketTest < Minitest::Test
 
     assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
   end
+
+  def test_vendors_that_sell
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
+  end
   
 end
