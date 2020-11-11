191ef959ece0b5a037f44eb49bb2dc034137f2d7
Aiden Mendez <aidenmendez@gmail.com>
2020-11-11 09:46:43 -0700

    Test it has attributes

--- test/market_test.rb
+++ test/market_test.rb
@@ -12,4 +12,8 @@ class MarketTest < Minitest::Test
   def test_it_exists
     assert_instance_of Market, @market
   end
+
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+  end
 end
