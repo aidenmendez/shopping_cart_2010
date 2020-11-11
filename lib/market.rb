edb2ba04c5c685126eb2e4b748deda496bb32f28
Aiden Mendez <aidenmendez@gmail.com>
2020-11-11 10:28:08 -0700

    Add total_inventory method

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,20 @@ class Market
       vendor.inventory.include?(item)
     end
   end
+
+  def total_inventory
+    result = {}
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, amount|
+        if result.key?(item)
+          result[item][:quantity] += amount
+          result[item][:vendors] << vendor
+        else
+          result[item] = {:quantity => amount, :vendors => [vendor]}
+        end
+      end
+    end
+
+    result
+  end
 end
