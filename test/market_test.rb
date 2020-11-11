715a41744baff2704c84718f283e743afcc7ebdc
Aiden Mendez <aidenmendez@gmail.com>
2020-11-11 09:48:23 -0700

    Test it has vendors attribute and reader

--- test/market_test.rb
+++ test/market_test.rb
@@ -15,5 +15,6 @@ class MarketTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
   end
 end
